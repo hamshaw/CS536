6f458f03acd9663b5c65a139831be6b92b14d4ab
Hannah Shaw <shawhan@mail.gvsu.edu>
2025-10-14 12:41:10 -0400

    creating suft and suftd

---
+++ lab3/v2/server/suft.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <signal.h>
+int main(int argc, char *argv[]){
+    
+    if (argc != 4){
+                printf("please enter <rcvip> <rcvport> <filename>\n");
+                exit(1);
+        }
+    
+    int pn = atoi(argv[1]);
+
+	struct sockaddr_in serveraddr, clientaddr;
+    memset(&serveraddr, 0, sizeof(serveraddr));
+
+
+	//socket()
+	if ((sd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+		printf("error creating socket\n");
+
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        serveraddr.sin_port = htons(pn);
+        serveraddr.sin_family = AF_INET;
+
+	//bind()
+	if ((bind = bind(sd, (struct sockaddr*)&serveraddr, sizeof(serveraddr))) == -1)
+		printf("error binding\n");
+
+
+	return 1;
+}
